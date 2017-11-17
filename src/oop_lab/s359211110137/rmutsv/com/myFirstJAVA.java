e7ab000f4187e7b9f07157be4348635ec0e8d057
bowaranya <bobow159@gmail.com>
2017-11-17 11:14:20 +0700

    successfuly infermation data

--- src/oop_lab/s359211110137/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110137/rmutsv/com/myFirstJAVA.java
@@ -4,6 +4,9 @@ public class myFirstJAVA {
 
     public static void main(String[] args) {
         System.out.println("Hello World, I'm BOW");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
     } //main
 
 }//class
