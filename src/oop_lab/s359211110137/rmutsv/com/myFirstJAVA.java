6041ab967a9d76fec00553a9b30290fbff0f6454
bowaranya <bobow159@gmail.com>
2017-11-17 10:32:11 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110137/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,9 @@
+package oop_lab.s359211110137.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, I'm BOW");
+    } //main
+
+}//class
