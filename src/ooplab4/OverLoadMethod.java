82257576ac5c22a5c1e6ffaac31dfa27a61cb48e
359211110137 <bobow159@gmail.com>
2017-12-20 17:12:09 +0700

    Overload Method

---
+++ src/ooplab4/OverLoadMethod.java
@@ -0,0 +1,27 @@
+package ooplab4;
+//1. Overload method
+//2. Overloading method
+//3. Method Overload
+//ความหมาย = เมธอดที่ชื่อเหมือนกัน แต่มีพารามิเตอร์ที่แตกต่างกัน
+public class OverLoadMethod {
+    public static void A(){
+        System.out.println("Hello A");
+    }
+    public static void A(int x){
+        System.out.println("Hello"+x);
+    }
+    public static void A (int x,int y){
+        System.out.println("Hello A"+(x+y));
+    }
+    public static void B(){
+        System.out.println("Hello B");
+        A();
+    }
+
+    public static void main(String[] args) {
+        A();
+        A(10);
+        A(10,20);
+        B();
+    }//main
+}//class
