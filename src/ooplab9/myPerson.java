007823007f5b232c030abbe55248fa3840714e70
359211110137 <bobow159@gmail.com>
2018-02-16 10:43:11 +0700

    myPerson

---
+++ src/ooplab9/myPerson.java
@@ -0,0 +1,15 @@
+package ooplab9;
+
+public class myPerson {
+    public static void main(String[] args) {
+
+        Person person = new Person("waranya", 23,
+                new Address("204","nakhonsrithammarat","80220"),
+                new Job("receptionist", 50000));
+
+
+        System.out.println(person.toString());
+
+
+    }//main
+}//class
